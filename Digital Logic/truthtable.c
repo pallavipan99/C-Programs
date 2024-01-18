61b80a1c55446faecc2dbaef58825eca396a5639
Pallavi <96259165+pallavipan99@users.noreply.github.com>
2024-01-18 12:00:00 -0500

    Add initial truth_table.c for digital logic

---
+++ Digital Logic/truthtable.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum { AND, OR, NAND, NOR, XOR, NOT, PASS, MULTIPLEXER, DECODER } gate_type;
+
+typedef struct{
+    gate_type type;
+    int size;          // indicates size of DECODER and MULTIPLEXER
+    int *inputs;       
+    int *outputs;      
+    int output;       
+} Gate;
+
+int num_inputs;
+int num_outputs;
+int num_variables = 0;
+int num_gates = 0;
+int input_variables[256];
+int output_variables[256];
+int values[256];  
+Gate gates[256];
+char variables[256][50];  
+int last_output = 0;
+
+int var(char *name) {
+    if(strcmp(name, "1") == 0){
+        return -2;
+    } 
+    if(strcmp(name, "0") == 0){
+        return -1;
+    }  
+    for(int x = 0; x < num_variables; x++){
+        if(strcmp(variables[x], name) == 0){
+            return x;
+        } 
+    }
+    strcpy(variables[num_variables], name);
+    return num_variables++;
+}
+
+int get_value(int index){
+    if(index == -1){
+        return 0;
+    }else if(index == -2){
+        return 1;
+    } 
+    return values[index];
+}
+
